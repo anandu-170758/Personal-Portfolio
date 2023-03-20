f2b76121322b4d741a8373d6af65ec4226a112cc
anandu_170758 <ananduksyam@gmail.com>
2023-03-21 02:25:55 +0530

    Project Card Component

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import ProjectItem from "../components/Projectitem"
+
+import "../styles/Project.css"
+import{ProjectList} from "../helpers/projectlist"
+
 
 function Projects() {
   return (
-    <div>Projects Page
+    <div className='projects'>
+      <h1>My Personal Projects</h1>
+      <div className='projecList'>
+       
+       {ProjectList.map((Project)=>{
+        return <ProjectItem name={Project.name} image={Project.Image}/>
+       })}
+      </div>
         
    </div>
   )
---
+++ src/helpers/projectlist.js
@@ -0,0 +1,20 @@
+import ab1 from "../assets/ab1.jpg"
+import ab2 from "../assets/ab2.jpg"
+import ab3 from "../assets/ab3.jpg"
+
+export const ProjectList =[
+    {
+        name:"hello",
+        Image:ab1,
+    },
+    {
+        name:"duck",
+        Image:ab2,
+    },
+    {
+        name:"zero",
+        Image:ab3,
+    },
+
+   
+]
