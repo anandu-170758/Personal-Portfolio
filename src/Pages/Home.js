4b74e4343053a6def3cf5dfa9d2c482ed505d772
anandu_170758 <ananduksyam@gmail.com>
2023-03-25 19:15:08 +0530

    style changed

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,38 +5,26 @@ import Instagram from '../SocialMedia/Instagram'
 import "../styles/Home.css"
 import Twitter from "../SocialMedia/Twitter"//import CV from '../SocialMedia/CV'
 import Email from '../SocialMedia/Email'
+import Skills from './skills'
 function Home() {
   return (
     <div className='home'>
       <div className='about'>
       <h3>Hi,My Name is Anandu K S!</h3>
+      <p>I'm a Software Developer </p>
       <div className='prompt'>
-        <p>I'm a Software Developer </p>
+       
         <Linkedin/>
        
         <Email/>
         <Twitter/>
         <GitHub/>
         <Instagram/>
-        
+       
         </div>
         </div>
-      <div className='skills'></div>
-      <h1>Skills</h1>
-      <ol className='list'>
-        <li classname="item">
-         <h2>Front-End</h2>
-         <span>ReactJS,HTML,CSS,BootStrap</span>
-        </li>
-        <li classname="item">
-         <h2>Back-End</h2>
-         <span>NodeJS,MySQL</span>
-        </li>
-        <li classname="item">
-         <h2>Languages</h2>
-         <span>JavaScript,Python</span>
-        </li>
-      </ol>
+        <Skills/>
+      
     </div>
   )
 }
