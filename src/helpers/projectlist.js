ab81f6cf04912712788bb5864280fca742e8da1c
anandu_170758 <ananduksyam@gmail.com>
2023-08-06 18:40:27 +0530

    Skills added in project

--- src/helpers/projectlist.js
+++ src/helpers/projectlist.js
@@ -1,12 +1,12 @@
 import ab1 from "../assets/ab1.jpg"
 import ab2 from "../assets/ab2.jpg"
-import ab3 from "../assets/ab3.jpg"
+
 
 export const ProjectList =[
     {
         name:"Covid19-Tracker",
         Image:ab1,
-        skills:"HTML,CSS,JavaScript",
+        skills:"ReactJS,HTML,CSS,JavaScript",
         url:"https://covid-tracker-ten-orcin.vercel.app/",
         githuburl:"https://github.com/anandu-170758/covid-tracker"
     
@@ -14,15 +14,11 @@ export const ProjectList =[
     {
         name:"QR-Code Generator",
         Image:ab2,
-        skills:"React JS,BootStrap",
+        skills:"HTML,CSS,JavaScript",
         url:"https://qr-code-generator-mauve.vercel.app/",
         githuburl:"https://github.com/anandu-170758/QR-Code-Generator"
     },
-    {
-        name:"zero",
-        Image:ab3,
-        skills:"HTML,CSS,JavaScript",
-    },
+    
 
    
 ]
