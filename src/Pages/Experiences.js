cac95033b8d73a10cfa615a2d037a988c10f33bb
anandu_170758 <ananduksyam@gmail.com>
2023-08-06 18:54:09 +0530

    Experience orderly arranged

--- src/Pages/Experiences.js
+++ src/Pages/Experiences.js
@@ -9,12 +9,12 @@ function Experiences() {
     <VerticalTimeline lineColor="#3e497a">
       <VerticalTimelineElement
       className="vertical-timeline-element--education"
-      date="2013-2014"
+      date="2017-2022"
       iconStyle={{background:"#3e497a",colo:"#fff"}}
       icon={<SchoolIcon/>}
       >
-        <h3 className="vertical-timeline-element-title font-fix-css">St. Thomas High School,Kallara,Kerala,India</h3>
-        <h3 className="font-fix-css">High School </h3>
+        <h3 className="vertical-timeline-element-title font-fix-css">National Institute of Technology,Calicut,India</h3>
+        <h3 className="font-fix-css">Bachelor of Technology,Electronics and Communication</h3>
       </VerticalTimelineElement>
 
       <VerticalTimelineElement
@@ -29,12 +29,12 @@ function Experiences() {
 
       <VerticalTimelineElement
       className="vertical-timeline-element--education"
-      date="2017-2022"
+      date="2013-2014"
       iconStyle={{background:"#3e497a",colo:"#fff"}}
       icon={<SchoolIcon/>}
       >
-        <h3 className="vertical-timeline-element-title font-fix-css">National Institute of Technology,Calicut,India</h3>
-        <h3 className="font-fix-css">Bachelor of Technology,Electronics and Communication</h3>
+        <h3 className="vertical-timeline-element-title font-fix-css">St. Thomas High School,Kallara,Kerala,India</h3>
+        <h3 className="font-fix-css">High School </h3>
       </VerticalTimelineElement>
 
 
