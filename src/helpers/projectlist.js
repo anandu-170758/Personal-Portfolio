63a7c94fad7593fda66a268821bfa89430e74b57
anandu_170758 <ananduksyam@gmail.com>
2023-08-07 01:02:09 +0530

    photo size update

--- src/helpers/projectlist.js
+++ src/helpers/projectlist.js
@@ -1,11 +1,11 @@
-// import ab1 from "../assets/ab1.jpg"//
+import ab1 from "../assets/ab1.jpg"//
 import ab2 from "../assets/ab2.jpg"
 
 
 export const ProjectList =[
     {
         name:"Covid19-Tracker",
-        Image:ab2,
+        Image:ab1,
         skills:"ReactJS,HTML,CSS,JavaScript",
         url:"https://covid-tracker-ten-orcin.vercel.app/",
         githuburl:"https://github.com/anandu-170758/covid-tracker",
